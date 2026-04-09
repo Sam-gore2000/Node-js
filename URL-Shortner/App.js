c1206e5741c3844d56eb5d08eba887bfc61a3281
Samadhan Gore <Samadhan.Gore@LionReachMedia.local>
2026-04-09 15:27:02 +0530

    Upload URL Shortner App

---
+++ URL-Shortner/App.js
@@ -0,0 +1,36 @@
+import { readFile } from 'fs/promises';
+import { createServer } from 'http';
+
+const server = createServer(async (req, res) => {
+
+  try {
+    if (req.method === "GET") {
+
+      if (req.url === "/") {
+        const data = await readFile("./index.html");
+        res.writeHead(200, { "Content-Type": "text/html" });
+        return res.end(data);
+      }
+
+      if (req.url === "/style.css") {
+        const data = await readFile("./style.css");
+        res.writeHead(200, { "Content-Type": "text/css" });
+        return res.end(data);
+      }
+
+      // 🔥 fallback (IMPORTANT)
+      res.writeHead(404);
+      res.end("Page Not Found");
+    }
+
+  } catch (error) {
+    console.log(error);
+    res.writeHead(500);
+    res.end("Server Error");
+  }
+
+});
+
+server.listen(3002, () => {
+  console.log("Running on http://localhost:3002");
+});
