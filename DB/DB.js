b869ae3c81b77cea89ad114fd1f9b2254f0ffe1a
samadhan Gore <samadhangore1910@gmail.com>
2026-05-21 11:35:27 +0530

    Upload crud operation

--- DB/DB.js
+++ DB/DB.js
@@ -10,14 +10,43 @@ try {
   const db = client.db("sam_user");
 
   const userCollection = db.collection("users");
+// Add Data
+  
+  // const result = await userCollection.insertOne({
+  //   name: "Sam Patil",
+  //   age: 26,
+  //   email: "sampatil1010@gmail.com",
+  // });
 
-  const result = await userCollection.insertOne({
-    name: "Sam Patil",
-    age: 26,
-    email: "sampatil1010@gmail.com",
-  });
 
-  console.log(result);
+//   const result = await userCollection.insertMany([{
+//     name: "Sam Patil",
+//     age: 26,
+//     email: "sampatil1010@gmail.com",
+//   },
+//   {
+//     name: "mamu Patil",
+//     age: 27,
+//     email: "mamupatil1010@gmail.com",
+//   },
+//   {
+//     name: "Member Patil",
+//     age: 29,
+//     email: "memberpatil1010@gmail.com",
+//   }
+// ]);
+
+
+// Read Data
+  //  const result=await  userCollection.find().toArray()
+  // console.log(result);
+
+  // update data
+
+  // userCollection.updateOne({name:"Member Patil"},{$set:{age:45}})
+
+  // Delete Data
+  userCollection.deleteOne({name:"Sam Patil"})
 
 } catch (error) {
   console.log(error);
